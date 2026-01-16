5daf847215dc6dff3ed997a7a7af0eb581bb9b74
itsaerosphere <itsaerosphere@gmail.com>
2026-01-15 19:52:34 -0500

    Now the website should be fixed... right?

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,8 @@ export default defineConfig({
     // basic site config
 
     // For site location config
-    site: "https://docs.spherico.one/wiki/", // Uncomment when DNS setup is done. 
+    site: "https://docs.spherico.one/", // Uncomment when DNS setup is done. 
+	base: "/wiki/",
 
     trailingSlash: "ignore",
     integrations: [
